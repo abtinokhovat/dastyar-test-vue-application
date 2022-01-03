76c0f57408f70ddf501bbcc21c91ed6e75f34425
Abtin Okhovat <abtinokhovat@gmail.com>
2022-01-03 19:21:00 +0330

    Adding project files and gitignore

---
+++ app.js
@@ -0,0 +1,21 @@
+async function getTasks() {
+    try {
+        const resp = await axios.get('http://localhost:5119/api/Task');
+        return (resp.data.succeeded) ? resp.data.response : resp.data.message;
+
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+function renderTasks(tasks) {
+    const ulTask = document.getElementById('task-ul');
+    for (let task of tasks) {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(task.name));
+        ulTask.appendChild(li);
+    }
+
+}
+
+getTasks().then(res => renderTasks(res));
